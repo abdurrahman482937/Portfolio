44c8a834745b59705bf75af66a9a06b6506410bd
Abdur Rahman <152638668+abdurrahman482937@users.noreply.github.com>
2025-03-11 18:45:44 +0600

    ....update

--- data/index.ts
+++ data/index.ts
@@ -205,13 +205,16 @@ export const socialMedia = [
   {
     id: 1,
     img: "/git.svg",
+    link: "https://github.com/abdurrahman482937"
   },
   {
     id: 2,
     img: "/twit.svg",
+    link: "https://github.com/abdurrahman482937"
   },
   {
     id: 3,
     img: "/link.svg",
+    link: "https://github.com/abdurrahman482937"
   },
 ];
